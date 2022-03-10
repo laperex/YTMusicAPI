ca00772e3e240a06d788337a3a3a54641197e509
TheSuperKDK <sreenikalarikkal@gmail.com>
2022-03-10 23:32:52 +0530

    fix login

--- YTMusicApi.py
+++ YTMusicApi.py
@@ -1,15 +1,29 @@
 from hashlib import sha1
 import time
 import requests
+import sqlite3
 
+# Tested on Mozilla FireFox
 
 class YTMusicAPI:
-	def __init__(self, __Secure_3PSID: str, __Secure_3PAPISID: str, x_origin: str, user_agent: str, key: str="AIzaSyC9XL3ZjWddXya6X74dJoCTL-WEYFDNX30"):
-		self.__Secure_3PSID = __Secure_3PSID
-		self.__Secure_3PAPISID = __Secure_3PAPISID
-		self.x_origin = x_origin
-		self.user_agent = user_agent
-		self.key = key
+	def __init__(self, profile_path: str=""):
+		self.__Secure_3PSID = ""
+		self.__Secure_3PAPISID = ""
+
+		if profile_path:
+			conn = sqlite3.connect(profile_path)
+			c = conn.cursor()
+
+			for row in c.execute('SELECT * FROM moz_cookies;'):
+				if row[4] == '.youtube.com' and row[2] == '__Secure-3PAPISID':
+					self.__Secure_3PAPISID = row[3]
+				if row[4] == '.youtube.com' and row[2] == '__Secure-3PSID':
+					self.__Secure_3PSID = row[3]
+			
+			c.close()
+			conn.close()
+		
+		self.authentication = self.google_auth()
 		self.payload = {
 			"context": {
 				"client": {
@@ -20,19 +34,21 @@ class YTMusicAPI:
 		}
 
 	def google_auth(self):
+		if not self.__Secure_3PAPISID:
+			return ""
 		sha_1 = sha1()
 		unix_timestamp = str(int(time.time()))
 		sha_1.update((unix_timestamp + ' ' + (self.__Secure_3PAPISID + ' ' + "https://music.youtube.com")).encode('utf-8'))
 		return "SAPISIDHASH " + unix_timestamp + "_" + sha_1.hexdigest()
 
-	def __request(self, url: str, referer: str="", data:dict={}):
-		
-		self.payload.update(data)
+	def __request(self, url: str, referer: str="", data:dict={}):		
+		if data:
+			self.payload.update(data)
 
-		return requests.post("https://music.youtube.com/youtubei/v1/" + url + "&key=" + self.key, headers={
-			'x-origin': self.x_origin,
-			'authorization': self.google_auth(),
-			'user-agent': self.user_agent,
+		return requests.post("https://music.youtube.com/youtubei/v1/" + url + "&key=AIzaSyC9XL3ZjWddXya6X74dJoCTL-WEYFDNX30", headers={
+			'x-origin': "https://music.youtube.com",
+			'authorization': self.authentication,
+			'user-agent': "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:86.0) Gecko/20100101 Firefox/86.0",
 			'referer': 'https://music.youtube.com/' + referer,
 			'cookie': '__Secure-3PSID=' + self.__Secure_3PSID + '; __Secure-3PAPISID=' + self.__Secure_3PAPISID,
 			
@@ -42,12 +58,11 @@ class YTMusicAPI:
 		res = self.__request("browse?", "playlist?list=" + id, {"browseId": "VL" + id}).json()
 
 		path = res['contents']['singleColumnBrowseResultsRenderer']['tabs'][0]['tabRenderer']['content']['sectionListRenderer']['contents'][0]['musicPlaylistShelfRenderer']['contents']
-		idx = 0
+		
 		while 'contents' in res or 'continuationContents' in res:
 			for i in range(len(path)):
-				idx += 1
 				try:
-					yield (idx, path[i]['musicResponsiveListItemRenderer']['playlistItemData']['videoId'])
+					yield path[i]['musicResponsiveListItemRenderer']['playlistItemData']['videoId']
 				except KeyError:
 					continue
 
@@ -77,8 +92,8 @@ class YTMusicAPI:
 			}
 		], "playlistId": playlist_id})
 
-	def clone_playlist(self, initial_playlist: str, target_playlist: str):
-		target = self.get_playlist_songs(target_playlist)
+	def copy_playlist(self, initial_playlist: str, target_playlist: str):
+		target = [i for i in self.get_playlist_songs(target_playlist)]
 
 		for song in self.get_playlist_songs(initial_playlist):
 			if song not in target:
@@ -86,6 +101,11 @@ class YTMusicAPI:
 	
 	def create_playlist(self, name: str, description: str="", privacy: int=3):	# Private - 3 | Unlisted - 2 | Public - 1
 		print(self.__request("playlist/create?", data={"title": name, "description": description, "privacyStatus": privacy}))
+
+	# No Sign In Required
 	
 	def get_music_data(self, song_id):
 		return self.__request("player?", data={"videoId": song_id}).json()['videoDetails']
+
+	def search_song(self, query: str):
+		return self.__request(query)
