10072e7260681257df079a79f56bc5bb47d5e20a
TheSuperKDK <sreenikalarikkal@gmail.com>
2022-03-10 23:34:41 +0530

    fin

--- main.py
+++
@@ -1,7 +0,0 @@
-from pprint import pprint
-from YTMusicApi import YTMusicAPI
-import json
-
-if __name__ == "__main__":
-	api = YTMusicAPI("C:\\Users\\krishnadev\\AppData\\Roaming\\Mozilla\\Firefox\\Profiles\\35m1uqu9.default-release\\cookies.sqlite")
-	
